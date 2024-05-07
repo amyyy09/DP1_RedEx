e7fc17719734c4ad9c6afd670931018abb17784c
Duvet05 <gonzalo.galvezc@pucp.edu.pe>
2024-05-07 01:14:42 -0500

    update

--- ProyectoDP1/src/main/java/src/service/FitnessEvaluatorService.java
+++ ProyectoDP1/src/main/java/src/service/FitnessEvaluatorService.java
@@ -3,7 +3,6 @@ package src.service;
 import src.model.*;
 
 import java.time.LocalDateTime;
-import java.time.OffsetDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -71,76 +70,78 @@ public class FitnessEvaluatorService {
     }
 
     public double fitness(Map<Paquete, RutaTiempoReal> particula, List<Aeropuerto> aeropuertos,
-            List<Vuelo> vuelosActivos) { //RUTA -> FITNESSVALUE
+            List<Vuelo> vuelosActivos) { // RUTA -> FITNESSVALUE
         double penalizacion = 0.0;
         Map<Integer, Integer> usoCapacidadVuelos = new HashMap<>();
         Map<String, TreeMap<LocalDateTime, Integer>> usoCapacidadAlmacenes = new HashMap<>();
-        int colisiones = 0;
 
         for (Entry<Paquete, RutaTiempoReal> entrada : particula.entrySet()) {
             // Paquete paquete = entrada.getKey();
             RutaTiempoReal ruta = entrada.getValue();
             List<Vuelo> vuelos = ruta.getVuelos();
-            //Vuelo _vuelosActivo = encontrarVueloActual(vuelosActivos, ruta);
-            //paquete.setStatus(3);
+            // Vuelo _vuelosActivo = encontrarVueloActual(vuelosActivos, ruta);
+            // paquete.setStatus(3);
 
-            for(int i=0; i<vuelos.size();i++){
+            for (int i = 0; i < vuelos.size(); i++) {
                 // agregamos un paquete a la capacidad del vuelo en usoCapacidadVuelos
-                usoCapacidadVuelos.put(vuelos.get(i).getIdVuelo(), usoCapacidadVuelos.getOrDefault(vuelos.get(i).getIdVuelo(), 0) + 1);
+                usoCapacidadVuelos.put(vuelos.get(i).getIdVuelo(),
+                        usoCapacidadVuelos.getOrDefault(vuelos.get(i).getIdVuelo(), 0) + 1);
                 String codigoIATAOrigen = vuelos.get(i).getPlanDeVuelo().getCodigoIATAOrigen();
                 String codigoIATADestino = vuelos.get(i).getPlanDeVuelo().getCodigoIATADestino();
                 // si no existe la clave en usoCapacidadAlmacenes la creamos
                 TreeMap<LocalDateTime, Integer> usoCapacidadAlmacenesOrigen = usoCapacidadAlmacenes
-                    .computeIfAbsent(codigoIATAOrigen, k -> new TreeMap<>());
-                
-                Almacen almacenOrigen = aeropuertos.stream().filter(a -> a.getCodigoIATA().equals(codigoIATAOrigen)).findFirst().get().getAlmacen();
+                        .computeIfAbsent(codigoIATAOrigen, k -> new TreeMap<>());
+
+                Almacen almacenOrigen = aeropuertos.stream().filter(a -> a.getCodigoIATA().equals(codigoIATAOrigen))
+                        .findFirst().get().getAlmacen();
                 // int capacidadOrigen = almacenOrigen.getCapacidad();
                 int ocupacionOrigen;
-                // verificar que en el map de usoCapacidadAlmacenesOrigen no exista fechas anteriores a la fecha actual, es un treemap ordenado por fecha
-                if(usoCapacidadAlmacenesOrigen.size() > 0){
+                // verificar que en el map de usoCapacidadAlmacenesOrigen no exista fechas
+                // anteriores a la fecha actual, es un treemap ordenado por fecha
+                if (usoCapacidadAlmacenesOrigen.size() > 0) {
                     LocalDateTime fechaAnterior = usoCapacidadAlmacenesOrigen.lowerKey(vuelos.get(i).getHoraSalida());
-                    if(fechaAnterior != null){
+                    if (fechaAnterior != null) {
                         ocupacionOrigen = usoCapacidadAlmacenesOrigen.get(fechaAnterior);
-                    }
-                    else{
+                    } else {
                         ocupacionOrigen = almacenOrigen.getCantPaquetes();
                     }
-                }
-                else{
+                } else {
                     ocupacionOrigen = almacenOrigen.getCantPaquetes();
                 }
 
-                usoCapacidadAlmacenesOrigen.put(vuelos.get(i).getHoraSalida(), ocupacionOrigen-1);
+                usoCapacidadAlmacenesOrigen.put(vuelos.get(i).getHoraSalida(), ocupacionOrigen - 1);
 
-                NavigableMap<LocalDateTime, Integer> tailMap = usoCapacidadAlmacenesOrigen.tailMap(vuelos.get(i).getHoraSalida(), false);
+                NavigableMap<LocalDateTime, Integer> tailMap = usoCapacidadAlmacenesOrigen
+                        .tailMap(vuelos.get(i).getHoraSalida(), false);
 
                 for (Map.Entry<LocalDateTime, Integer> entry : tailMap.entrySet()) {
                     usoCapacidadAlmacenesOrigen.put(entry.getKey(), entry.getValue() - 1);
                 }
 
-                Almacen almacenDestino = aeropuertos.stream().filter(a -> a.getCodigoIATA().equals(codigoIATADestino)).findFirst().get().getAlmacen();
+                Almacen almacenDestino = aeropuertos.stream().filter(a -> a.getCodigoIATA().equals(codigoIATADestino))
+                        .findFirst().get().getAlmacen();
                 // int capacidadDestino = almacenDestino.getCapacidad();
                 int ocupacionDestino;
                 // si no existe la clave en usoCapacidadAlmacenes la creamos
                 TreeMap<LocalDateTime, Integer> usoCapacidadAlmacenesDestino = usoCapacidadAlmacenes
-                    .computeIfAbsent(codigoIATADestino, k -> new TreeMap<>());
-                // verificar que en el map de usoCapacidadAlmacenesDestino no exista fechas anteriores a la fecha actual, es un treemap ordenado por fecha
-                if(usoCapacidadAlmacenesDestino.size() > 0){
+                        .computeIfAbsent(codigoIATADestino, k -> new TreeMap<>());
+                // verificar que en el map de usoCapacidadAlmacenesDestino no exista fechas
+                // anteriores a la fecha actual, es un treemap ordenado por fecha
+                if (usoCapacidadAlmacenesDestino.size() > 0) {
                     LocalDateTime fechaAnterior = usoCapacidadAlmacenesDestino.lowerKey(vuelos.get(i).getHoraLlegada());
-                    if(fechaAnterior != null){
+                    if (fechaAnterior != null) {
                         ocupacionDestino = usoCapacidadAlmacenesDestino.get(fechaAnterior);
-                    }
-                    else{
+                    } else {
                         ocupacionDestino = almacenDestino.getCantPaquetes();
                     }
-                }
-                else{
+                } else {
                     ocupacionDestino = almacenDestino.getCantPaquetes();
                 }
 
-                usoCapacidadAlmacenesDestino.put(vuelos.get(i).getHoraLlegada(), ocupacionDestino+1);
+                usoCapacidadAlmacenesDestino.put(vuelos.get(i).getHoraLlegada(), ocupacionDestino + 1);
 
-                NavigableMap<LocalDateTime, Integer> tailMapDestino = usoCapacidadAlmacenesDestino.tailMap(vuelos.get(i).getHoraLlegada(), false);
+                NavigableMap<LocalDateTime, Integer> tailMapDestino = usoCapacidadAlmacenesDestino
+                        .tailMap(vuelos.get(i).getHoraLlegada(), false);
 
                 for (Map.Entry<LocalDateTime, Integer> entry : tailMapDestino.entrySet()) {
                     usoCapacidadAlmacenesDestino.put(entry.getKey(), entry.getValue() + 1);
@@ -148,7 +149,6 @@ public class FitnessEvaluatorService {
             }
 
         }
-
 
         // Verificar capacidad de vuelo
         for (Map.Entry<Integer, Integer> entry : usoCapacidadVuelos.entrySet()) {
@@ -165,7 +165,8 @@ public class FitnessEvaluatorService {
         for (Map.Entry<String, TreeMap<LocalDateTime, Integer>> entry : usoCapacidadAlmacenes.entrySet()) {
             String key = entry.getKey();
             TreeMap<LocalDateTime, Integer> value = entry.getValue();
-            Almacen almacen = aeropuertos.stream().filter(a -> a.getCodigoIATA().equals(key)).findFirst().get().getAlmacen();
+            Almacen almacen = aeropuertos.stream().filter(a -> a.getCodigoIATA().equals(key)).findFirst().get()
+                    .getAlmacen();
             for (Map.Entry<LocalDateTime, Integer> entry2 : value.entrySet()) {
                 Integer value2 = entry2.getValue();
                 if (value2 > almacen.getCapacidad()) {
@@ -179,7 +180,7 @@ public class FitnessEvaluatorService {
         return fitnessValue;
     }
 
-    private Vuelo encontrarVueloActual(List<Vuelo> vuelosActivos, RutaTiempoReal ruta) {
+    public Vuelo encontrarVueloActual(List<Vuelo> vuelosActivos, RutaTiempoReal ruta) {
         for (Vuelo vuelo : vuelosActivos) {
             for (Vuelo vueloEnRuta : ruta.getVuelos()) {
                 if (vuelo.getIdVuelo() == vueloEnRuta.getIdVuelo()) {
@@ -190,7 +191,7 @@ public class FitnessEvaluatorService {
         return null; // Retornar null si no se encuentra ningún vuelo que coincida
     }
 
-    private Almacen encontrarAlmacenActual(List<Aeropuerto> aeropuertos, String codigoIATA) {
+    public Almacen encontrarAlmacenActual(List<Aeropuerto> aeropuertos, String codigoIATA) {
         for (Aeropuerto aeropuerto : aeropuertos) {
             if (aeropuerto.getCodigoIATA().equals(codigoIATA)) {
                 return aeropuerto.getAlmacen();
