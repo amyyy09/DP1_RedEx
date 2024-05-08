38fd8a1af3c327738bafe3fefcc5a50a7879a0ee
Daniel <ponce.daniel@pucp.edu.pe>
2024-05-07 21:38:30 -0500

    Ver tiempos de genetico

--- ProyectoDP1/src/main/java/src/main/algoritmoGenetico.java
+++ ProyectoDP1/src/main/java/src/main/algoritmoGenetico.java
@@ -23,14 +23,22 @@ public class algoritmoGenetico {
 			List<Aeropuerto> aeropuertos = DatosAeropuertos.getAeropuertosInicializados();//Realizar lectura de datos
 			List<Envio> envios = vueloService.getEnvios(archivoRutaEnvios);
 			List<PlanDeVuelo> planesDeVuelo = vueloService.getPlanesDeVuelo(aeropuertos, archivoRutaPlanes);// en planes de vuelo se tiene la hora y su GMT
+			List<Paquete> paquetes = envios.stream().map(Envio::getPaquetes).flatMap(List::stream).collect(Collectors.toList());
+            System.out.println("Paquetes: " + paquetes.size());
+
+            System.out.println("Empezando a generar rutas predefinidas... en el tiempo de ejecución: " + System.currentTimeMillis());
 			List<RutaPredefinida> rutasPred = planificacionService.generarRutas(aeropuertos, planesDeVuelo);
+			System.out.println("Rutas predefinidas generadas." + System.currentTimeMillis());
 			List<Almacen> almacenes = aeropuertos.stream().map(Aeropuerto::getAlmacen).collect(Collectors.toList());
 			List<Vuelo> vuelosActuales = vueloService.getVuelosActuales(planesDeVuelo);
+			
+			
+			System.out.println("Empezando a ejecutar genetico.. en el tiempo de ejecución: " + System.currentTimeMillis());
 			if (!envios.isEmpty()) {
 				System.err.println("Inicio de la ejecución del algoritmo genético.");
 				Cromosoma resultado = planificacionService.ejecutarAlgoritmoGenetico(envios, aeropuertos, planesDeVuelo,rutasPred,almacenes,vuelosActuales);
 				if (resultado != null) {
-					System.out.println("Resultado del algoritmo genético procesado.");
+					System.out.println("Resultado del genetico procesado en el tiempo de ejecución: " + System.currentTimeMillis());
 				} else {
 					System.out.println("No se obtuvo un resultado válido del algoritmo genético.");
 				}
