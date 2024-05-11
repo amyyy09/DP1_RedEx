bcf8f0399f182d563c1184530b4327793fa91dc2
DanielPonce <Danielponce@belcorp.biz>
2024-05-11 16:47:59 -0500

    avance de bd

--- ProyectoDP1/src/main/java/src/dto/AeropuertoDTO.java
+++ ProyectoDP1/src/main/java/src/dto/AeropuertoDTO.java
@@ -2,16 +2,63 @@ package src.dto;
 
 import java.time.LocalDateTime;
 
-public class AeropuertoDTO {
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.Lob;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "aeropuerto")
+@SQLDelete(sql = "UPDATE aeropuerto SET activo = 0 WHERE id = ?")
+@Where(clause = "activo = 1")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+
+public class AeropuertoDTO extends BaseEntity {
+    @Column(name = "id_aeropuerto")
     private String idAeropuerto;
+    
+    @Column(name = "ciudad")
     private String ciudad;
+
+    @Column(name = "pais")
     private String pais;
+
+    @Column(name = "continente")
     private String continente;
+
+    @Column(name = "alias")
     private String alias;
+
+    @Column(name = "zonaHorariaGTM")
     private int zonaHorariaGTM;
+
+    @Column(name = "longitud")
     private double longitud;
+
+    @Column(name = "latitud")
     private double latitud; 
+
+    @Column(name = "capacidad")
     private long capacidad;
+
+    @Column(name = "cantPaquetes")
     private int cantPaquetes;
     
 }
--- ProyectoDP1/src/main/java/src/dto/VueloDTO.java
+++ ProyectoDP1/src/main/java/src/dto/VueloDTO.java
@@ -2,10 +2,54 @@ package src.dto;
 
 import java.time.LocalDateTime;
 
-public class VueloDTO {
+import java.time.OffsetDateTime;
+
+import java.time.LocalDateTime;
+import java.time.OffsetTime;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.Lob;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "vuelo")
+@SQLDelete(sql = "UPDATE vuelo SET activo = 0 WHERE id = ?")
+@Where(clause = "activo = 1")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+
+public class VueloDTO extends BaseEntity{
+    @Column(name = "idVuelo")
     private String idVuelo;
+
+    @Column(name = "cantPaquetes")
     private int cantPaquetes;
+
+    @Column(name = "capacidad")
     private int capacidad;
-    private LocalDateTime fecha; 
+
+    @Column(name = "fecha")
+    private LocalDateTime fecha;
+    
+    @Column(name = "activo")
     private boolean activo;
 }
