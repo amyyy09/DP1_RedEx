09a9e514384f0584415ea45ef9b3309ec45b7f15
Duvet05 <gonzalo.galvezc@pucp.edu.pe>
2024-05-20 21:08:53 -0500

    update

---
+++ ProyectoDP1/src/main/java/src/main/RedexApplication.java
@@ -0,0 +1,12 @@
+package src.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedexApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(RedexApplication.class, args);
+		System.out.println("Gonzalo");
+	}
+}
