63939f607752766dd036935c516d237721ce31b7
Duvet05 <gonzalo.galvezc@pucp.edu.pe>
2024-05-22 07:09:45 -0500

    .

--- ProyectoDP1/src/main/java/src/model/PlanDeVuelo.java
+++ ProyectoDP1/src/main/java/src/model/PlanDeVuelo.java
@@ -3,6 +3,7 @@ package src.model;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
+import src.entity.PlanDeVueloEntity;
 import lombok.NoArgsConstructor;
 import java.time.OffsetTime;
 
@@ -17,4 +18,15 @@ public class PlanDeVuelo {
         private OffsetTime horaLlegada;
         private int capacidad;
         private boolean isSameContinent;
+
+        public static PlanDeVuelo convertirPlanDeVueloFromEntity(PlanDeVueloEntity entity) {
+                PlanDeVuelo plan = new PlanDeVuelo();
+                plan.setCodigoIATAOrigen(entity.getCodigoIATAOrigen());
+                plan.setCodigoIATADestino(entity.getCodigoIATADestino());
+                plan.setHoraSalida(entity.getHoraSalida());
+                plan.setHoraLlegada(entity.getHoraLlegada());
+                plan.setCapacidad(entity.getCapacidad());
+                // Assume other fields and initialization
+                return plan;
+        }
 }
