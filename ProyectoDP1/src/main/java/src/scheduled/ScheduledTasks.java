b483441e8a41936bf6caa8e8ae663afa12c397dc
Duvet05 <gonzalo.galvezc@pucp.edu.pe>
2024-05-21 21:58:00 -0500

    n

--- ProyectoDP1/src/main/java/src/scheduled/ScheduledTasks.java
+++ ProyectoDP1/src/main/java/src/scheduled/ScheduledTasks.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class ScheduledTasks {
-
+    // se corre cada 5 minutos
     @Scheduled(fixedRate = 5000)
     public void performTask() {
         System.out.println("Regular task performed using Spring Scheduler.");
