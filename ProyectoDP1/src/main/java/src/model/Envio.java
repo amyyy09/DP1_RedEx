4163a73b9dc19fd521b5c3467549919ebbc2b78e
Duvet05 <gonzalo.galvezc@pucp.edu.pe>
2024-05-21 22:26:15 -0500

    update

--- ProyectoDP1/src/main/java/src/model/Envio.java
+++ ProyectoDP1/src/main/java/src/model/Envio.java
@@ -1,10 +1,15 @@
 package src.model;
 
 import java.util.List;
+import java.util.stream.Collectors;
 import java.time.LocalDateTime;
 
 import lombok.Getter;
 import lombok.Setter;
+import src.entity.AeropuertoEntity;
+import src.entity.EnvioEntity;
+import src.entity.PaqueteEntity;
+import src.service.AeropuertoService;
 import lombok.NoArgsConstructor;
 import lombok.AllArgsConstructor;
 
@@ -21,4 +26,22 @@ public class Envio {
     private String codigoIATADestino;
     private int cantPaquetes;
     private List<Paquete> paquetes;
+
+    public static EnvioEntity convertirAeropuetoToDTO(Envio envio) {
+        EnvioEntity envioEntity = new EnvioEntity();
+        envioEntity.setIdEnvio(envio.getIdEnvio());
+        envioEntity.setFechaHoraOrigen(envio.getFechaHoraOrigen());
+        envioEntity.setZonaHorariaGMT(envio.getZonaHorariaGMT());
+
+        // Assume AeropuertoEntity has a static method to fetch by IATA code
+
+        envioEntity.getAeropuertoOrigen().setCodigoIATA(envio.getCodigoIATAOrigen());
+        envioEntity.getAeropuertoDestino().setCodigoIATA(envio.getCodigoIATADestino());
+
+        envioEntity.setCantPaquetes(envio.getCantPaquetes());
+
+        // Convert Paquetes list
+
+        return envioEntity;
+    }
 }
