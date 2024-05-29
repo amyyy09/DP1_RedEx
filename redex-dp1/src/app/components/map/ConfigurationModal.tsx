a090df7837c750ef53acea71fabdb13df225ace4
Sergio Escalante Gonzales <sergio.escalante@pucp.edu.pe>
2024-05-29 16:53:41 -0500

    JSON ya llega a Front

--- redex-dp1/src/app/components/map/ConfigurationModal.tsx
+++ redex-dp1/src/app/components/map/ConfigurationModal.tsx
@@ -1,7 +1,30 @@
 'use client';
-
 import React, { useState } from 'react';
 import '../../styles/ConfigurationModal.css';
+
+class Vuelo {
+  cantPaquetes: number;
+  capacidad: number;
+  status: number;
+  indexPlan: number;
+  horaSalida: Date;
+  horaLlegada: Date;
+  aeropuertoOrigen: string;
+  aeropuertoDestino: string;
+  idVuelo: string;
+
+  constructor(data: any) {
+    this.cantPaquetes = data.cantPaquetes;
+    this.capacidad = data.capacidad;
+    this.status = data.status;
+    this.indexPlan = data.indexPlan;
+    this.horaSalida = new Date(data.horaSalida[0], data.horaSalida[1] - 1, data.horaSalida[2], data.horaSalida[3], data.horaSalida[4]);
+    this.horaLlegada = new Date(data.horaLlegada[0], data.horaLlegada[1] - 1, data.horaLlegada[2], data.horaLlegada[3], data.horaLlegada[4]);
+    this.aeropuertoOrigen = data.aeropuertoOrigen;
+    this.aeropuertoDestino = data.aeropuertoDestino;
+    this.idVuelo = data.idVuelo;
+  }
+}
 
 interface ConfigurationModalProps {
   onApply: () => void;
@@ -11,6 +34,8 @@ const ConfigurationModal: React.FC<ConfigurationModalProps> = ({ onApply }) => {
   const [simulationMode, setSimulationMode] = useState('');
   const [startDate, setStartDate] = useState('');
   const [startTime, setStartTime] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [vuelos, setVuelos] = useState([]);
 
   const handleModeChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
     setSimulationMode(e.target.value);
@@ -21,8 +46,8 @@ const ConfigurationModal: React.FC<ConfigurationModalProps> = ({ onApply }) => {
   };
 
   
-  const handleTimeChange = (event) => {
-    setStartTime(event.target.value);
+  const handleTimeChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setStartTime(e.target.value);
   };
 
 
@@ -32,9 +57,9 @@ const ConfigurationModal: React.FC<ConfigurationModalProps> = ({ onApply }) => {
     onApply();
   };
 
-  const formatDateTime = (date, time) => {
+  const formatDateTime = (date: Date, time: string) => {
     const [hours, minutes] = time.split(':');
-    date.setHours(hours, minutes);
+    date.setHours(parseInt(hours), parseInt(minutes));
     const year = date.getFullYear();
     const month = String(date.getMonth() + 1).padStart(2, '0');
     const day = String(date.getDate()).padStart(2, '0');
@@ -45,6 +70,7 @@ const ConfigurationModal: React.FC<ConfigurationModalProps> = ({ onApply }) => {
   };
 
   const handleApplyClick = async () => {
+    setLoading(true);
     const selectedDate = new Date(startDate);
     const formattedDate = formatDateTime(selectedDate, startTime);
 
@@ -69,12 +95,39 @@ const ConfigurationModal: React.FC<ConfigurationModalProps> = ({ onApply }) => {
       }
 
       const responseData = await response.json();
-      console.log('Response:', responseData);
-      // Maneja la respuesta según sea necesario
+      const vuelosData = [];
+
+      // Procesar los vuelos desde el responseData
+      for (const key in responseData) {
+        if (responseData.hasOwnProperty(key)) {
+          const paquete = responseData[key];
+          paquete.vuelos.forEach(vueloData => {
+            const vuelo = new Vuelo({
+              ...vueloData,
+              aeropuertoOrigen: paquete.aeropuertoOrigen,
+              aeropuertoDestino: paquete.aeropuertoDestino
+            });
+            vuelosData.push(vuelo);
+          });
+        }
+      }
+
+      setVuelos(vuelosData);
+      console.log('Vuelos:', vuelosData);
     } catch (error) {
       console.error('Error:', error);
+    } finally {
+      setLoading(false); // Desactivar estado de cargando
     }
   };
+
+  if (loading) {
+    return (
+      <div className="loading-overlay">
+        <div className="loading-spinner">Cargando...</div>
+      </div>
+    );
+  }
 
   return (
     <div className="modal-overlay">
