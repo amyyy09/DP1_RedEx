1f1002ffc13273cebc4a8ea54183be0366eb5969
Gonzalo <69012359+Duvet05@users.noreply.github.com>
2024-06-02 19:35:28 -0500

    update carga de datos

--- ProyectoDP1/src/main/java/src/service/EnvioService.java
+++ ProyectoDP1/src/main/java/src/service/EnvioService.java
@@ -10,17 +10,13 @@ import src.repository.EnvioRepository;
 import src.repository.PaqueteRepository;
 import src.utility.ConversionesModelDTO;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.IOException;
 import java.time.LocalDateTime;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.stream.Collectors;
 
 import src.services.*;
-import src.model.*;
 
 import javax.annotation.PostConstruct;
 
@@ -31,23 +27,23 @@ public class EnvioService {
 
     @Autowired
     private PaqueteRepository paqueteRepository;
-    
+
     @Autowired
     private ConversionesModelDTO conversionesModelDTO;
 
     VueloServices vueloService = new VueloServices();
-    
+
     @Autowired
     private AeropuertoService aeropuertoService;
 
     private List<Envio> envios;
-    private final String archivoRutaEnvios = "ProyectoDP1/src/main/resources/combined.txt" ;
+    private final String archivoRutaEnvios = "ProyectoDP1/src/main/resources/combined.txt";
 
     @PostConstruct
     public void init() {
         envios = new CopyOnWriteArrayList<>();
         try {
-            envios=vueloService.getEnvios(archivoRutaEnvios);
+            envios = vueloService.getEnvios(archivoRutaEnvios);
             System.out.println("Se cargaron los envios");
         } catch (IOException e) {
             // Maneja la excepción de acuerdo a tus necesidades
@@ -62,11 +58,12 @@ public class EnvioService {
     public List<Envio> getEnviosPorFechaHora(LocalDateTime fechaHora) {
         LocalDateTime fechaHoraFin = fechaHora.plusHours(2);
         return envios.stream()
-                     .filter(envio -> {
-                        LocalDateTime fechaHoraGMT0 = convertirAGMT0(envio.getFechaHoraOrigen(), envio.getCodigoIATAOrigen());
-                        return !fechaHoraGMT0.isBefore(fechaHora) && fechaHoraGMT0.isBefore(fechaHoraFin);
-                    })
-                    .collect(Collectors.toList());
+                .filter(envio -> {
+                    LocalDateTime fechaHoraGMT0 = convertirAGMT0(envio.getFechaHoraOrigen(),
+                            envio.getCodigoIATAOrigen());
+                    return !fechaHoraGMT0.isBefore(fechaHora) && fechaHoraGMT0.isBefore(fechaHoraFin);
+                })
+                .collect(Collectors.toList());
     }
 
     public LocalDateTime convertirAGMT0(LocalDateTime fechaHora, String codigoIATAOrigen) {
@@ -74,16 +71,15 @@ public class EnvioService {
         return fechaHora.minusHours(zonaHorariaGMT);
     }
 
-    public void guardarEnvios (List<Envio> envios) {
-        //conversionesModelDTO.convertirEnviosToDTO(envios);
-        
-        
+    public void guardarEnvios(List<Envio> envios) {
+        // conversionesModelDTO.convertirEnviosToDTO(envios);
+
         // List<EnvioDTO> enviosDTO = conversionesModelDTO.convertirEnviosToDTO(envios);
         // for (EnvioDTO envioDTO : enviosDTO) {
-        //     envioRepository.save(envioDTO);
-        //     for (PaqueteDTO paqueteDTO : envioDTO.getPaquetes()) {
-        //         paqueteRepository.save(paqueteDTO);
-        //     }
+        // envioRepository.save(envioDTO);
+        // for (PaqueteDTO paqueteDTO : envioDTO.getPaquetes()) {
+        // paqueteRepository.save(paqueteDTO);
+        // }
         // }
 
         List<EnvioDTO> enviosDTO = conversionesModelDTO.convertirEnviosToDTO(envios);
@@ -96,5 +92,5 @@ public class EnvioService {
             paqueteRepository.saveAll(envioDTO.getPaquetes());
         }
     }
-    
+
 }
