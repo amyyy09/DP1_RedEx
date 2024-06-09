34b713df36ef66ab864792ca2210b4c52c7979b4
Daniel <ponce.daniel@pucp.edu.pe>
2024-06-09 15:32:41 -0500

    a

--- python_ayuda/sparkBD.py
+++ python_ayuda/sparkBD.py
@@ -1,6 +1,8 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import split, col, concat_ws, monotonically_increasing_id, current_timestamp, lit, to_timestamp, date_format
 from pyspark.sql.types import IntegerType
+from pyspark.sql.functions import to_date
+import time
 
 # Configurar SparkSession
 spark = SparkSession.builder \
@@ -10,11 +12,11 @@ spark = SparkSession.builder \
 
 
 # Leer el archivo CSV en un DataFrame de Spark
-file_path = "python_ayuda/enviado/pack_enviado_EHAM.txt"
+file_path = "python_ayuda/combined.txt"
 df = spark.read.csv(file_path)
 
 # Mostrar un ejemplo del DataFrame
-df.show(5, truncate=False)
+# df.show(5, truncate=False)
 
 # Dividir la columna en varias columnas usando expresiones regulares
 split_col = split(df['_c0'], '-')
@@ -33,23 +35,33 @@ df_size = df.count()
 print(f"El tamaño del DataFrame antes del filtrado es: {df_size} filas")
 
 
-# Crear la columna de fecha y hora combinada
-df = df.withColumn('fechaHoraOrigen', concat_ws(' ', df['FechaEnvio'], df['HoraEnvio']))
-# Verificar la columna concatenada
-df.select('fechaHoraOrigen').show(10, truncate=False)
-# Convertir la columna de fecha y hora a timestamp incluyendo milisegundos
-df = df.withColumn('fechaHoraOrigen', to_timestamp(col('fechaHoraOrigen'), 'yyyyMMdd HH:mm:ss.00000'))
+# # Crear la columna de fecha y hora combinada
+# df = df.withColumn('fechaHoraOrigen', concat_ws(' ', df['FechaEnvio'], df['HoraEnvio']))
 
 
+# Crear la columna de fecha y hora combinada, añadiendo segundos y milisegundos ficticios
+# Crear la columna de fecha y hora combinada en el formato 'yyyy-MM-dd HH:mm:ss.SSSSSS'
+df = df.withColumn('fechaHoraOrigen', concat_ws(' ', 
+    concat_ws('-', df['FechaEnvio'].substr(1, 4), df['FechaEnvio'].substr(5, 2), df['FechaEnvio'].substr(7, 2)), 
+    concat_ws(':', df['HoraEnvio'].substr(1, 2), df['HoraEnvio'].substr(4, 2), lit('00.000000'))
+))
+# df.select('fechaHoraOrigen').show(10, truncate=False)
 
+# Convertir la columna de fecha y hora a tipo TimestampType con el formato adecuado
+df = df.withColumn('fechaHoraOrigen', to_timestamp(col('fechaHoraOrigen'), 'yyyy-MM-dd HH:mm:ss.SSSSSS'))
+# df.select('fechaHoraOrigen').show(10, truncate=False)
 # Obtener la fecha actual en formato 'yyyyMMdd'
 current_date_str = date_format(current_timestamp(), 'yyyyMMdd')
+
+
+
 # Filtrar los envíos con fechaHoraOrigen mayor o igual al día de hoy
-df = df.filter(col('FechaEnvio') >= current_date_str)
+# df = df.filter(col('FechaEnvio') >= current_date_str)
+df = df.filter(date_format(col('fechaHoraOrigen'), 'yyyy-MM-dd') == '2024-05-25')
 # Obtener el tamaño del DataFrame filtrado
 df_size = df.count()
 print(f"El tamaño del DataFrame filtrado es: {df_size} filas")
-df.show(10, truncate=False)
+# df.show(10, truncate=False)
 
 
 # Asignar valores adicionales para los nuevos campos
@@ -81,7 +93,7 @@ envios_df = df.select(
 
 # Añadir una columna ID única para cada fila utilizando monotonically_increasing_id y transformándola en int
 
-envios_df.show(10, truncate=False)
+# envios_df.show(10, truncate=False)
 
 # Reparticionar el DataFrame
 num_partitions = 2048   # Puedes ajustar este número según los recursos disponibles
@@ -100,7 +112,10 @@ connection_properties = {
 }
 
 # Escribir el DataFrame en la base de datos MySQL
+# Medir el tiempo de carga
+start_time = time.time()
 try:
+    
     # Guardar los datos en la tabla ENVIOS usando múltiples hilos
     envios_df.write \
         .jdbc(url=jdbc_url, table="envio", mode="append", properties=connection_properties)
@@ -111,6 +126,10 @@ try:
     print("Datos cargados exitosamente en la base de datos MySQL")
 except Exception as e:
     print(f"Error al cargar datos en la base de datos MySQL: {e}")
+
+end_time = time.time()
+load_duration = end_time - start_time
+print(f"Tiempo de carga a la base de datos: {load_duration} segundos")
 
 # Detener SparkSession
 spark.stop()
