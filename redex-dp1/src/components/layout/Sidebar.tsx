9b9a7b82301c72c4efd777bfe8da231e46e96e6b
Gonzalo <69012359+Duvet05@users.noreply.github.com>
2024-06-12 04:53:26 -0500

    update

--- redex-dp1/src/components/layout/Sidebar.tsx
+++ redex-dp1/src/components/layout/Sidebar.tsx
@@ -1,25 +1,36 @@
-// components/Sidebar.tsx
+"use client";
 import React from "react";
+import next from "next";
+import { useRouter } from "next/navigation";
 import "../../styles/Sidebar.css";
+
 const Sidebar: React.FC = () => {
+  const router = useRouter();
+
   return (
     <div className="sidebar">
-      <div className="sidebar-item">
+      <div className="sidebar-item" onClick={() => router.push("/")}>
         <img src="./icons/hogar.png" alt="Home" />
       </div>
-      <div className="sidebar-item active">
+      <div
+        className="sidebar-item active"
+        onClick={() => router.push("/flights")}
+      >
         <img src="./icons/modo-vuelo.png" alt="Plane" />
       </div>
-      <div className="sidebar-item">
+      <div className="sidebar-item" onClick={() => router.push("/simulacion")}>
         <img src="./icons/radar.png" alt="Power" />
       </div>
-      <div className="sidebar-item">
-        <img src="./icons/paquete.png" alt="Package" />
+      <div className="sidebar-item" onClick={() => router.push("/pedidos")}>
+        <img src="./icons/paquete.png" alt="Pedidos" />
       </div>
-      <div className="sidebar-item">
+      <div
+        className="sidebar-item"
+        onClick={() => router.push("/configuracion")}
+      >
         <img src="./icons/actualizacion.png" alt="Loading" />
       </div>
-      <div className="sidebar-item">
+      <div className="sidebar-item" onClick={() => router.push("/salir")}>
         <img src="./icons/salir.png" alt="Logout" />
       </div>
     </div>
