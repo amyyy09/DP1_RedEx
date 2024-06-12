9e08918840230efc32839047cdd7deba7f76f2f0
Gonzalo <69012359+Duvet05@users.noreply.github.com>
2024-06-12 07:34:58 -0500

    update fixed map size

--- redex-dp1/src/app/page.tsx
+++ redex-dp1/src/app/page.tsx
@@ -5,7 +5,14 @@ import SimulationPage from "@/components/SimulationPage";
 
 const SimulacionPage: React.FC = () => {
   return (
-    <div style={{ display: "flex", flexDirection: "column" }}>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        height: "100vh",
+        width: "100vw",
+      }}
+    >
       <Topbar />
       <div style={{ display: "flex", flex: 1 }}>
         <SimulationPage />
