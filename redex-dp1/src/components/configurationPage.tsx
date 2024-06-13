d0fba45af385cdf18c749f62c036b8fdd873dce4
Gonzalo <69012359+Duvet05@users.noreply.github.com>
2024-06-12 19:23:36 -0500

    .

--- redex-dp1/src/components/configurationPage.tsx
+++ redex-dp1/src/components/configurationPage.tsx
@@ -7,19 +7,19 @@ const ConfigPage: React.FC = () => {
       <h1 className="config-title">Configuración y Carga de Datos</h1>
       <div className="config-buttons">
         <div className="config-button">
-          <img src="/icons/flight-plans.png" alt="Carga de Planes de Vuelo" />
+          <img src="config.png" alt="Carga de Planes de Vuelo" />
           <p>Carga de Planes de Vuelo</p>
         </div>
         <div className="config-button">
-          <img src="/icons/warehouses.png" alt="Carga de Almacenes" />
+          <img src="config.png" alt="Carga de Almacenes" />
           <p>Carga de Almacenes</p>
         </div>
         <div className="config-button">
-          <img src="/icons/historical-data.png" alt="Carga de Data Historica" />
+          <img src="config.png" alt="Carga de Data Historica" />
           <p>Carga de Data Historica</p>
         </div>
         <div className="config-button">
-          <img src="/icons/parameters.png" alt="Configuración de Parámetros" />
+          <img src="config.png" alt="Configuración de Parámetros" />
           <p>Configuración de Parámetros</p>
         </div>
       </div>
